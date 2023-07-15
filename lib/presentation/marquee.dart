65f85975d9d2ca49ffd92a52818568dd50f9c4f4
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-14 22:25:54 -0300

    feat(marquee): add conditional heading using blocBuilder

--- lib/presentation/marquee.dart
+++ lib/presentation/marquee.dart
@@ -1,27 +1,39 @@
 import 'package:automata_simulator/presentation/widgets/inline_painter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../application/canvas_cubit/canvas_cubit.dart';
 
 class Marquee extends StatelessWidget {
-  const Marquee({
-    super.key,
-    required this.start,
-    required this.end,
-  });
-
-  final Offset start, end;
+  const Marquee({super.key});
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    return CustomPaint(painter: InlinePainter(
-      builder: (canvas, rect) {
-        final paint = Paint()
-          ..color = colors.secondary.withOpacity(0.3)
-          ..style = PaintingStyle.fill;
+    final canvas = context.read<CanvasCubit>();
 
-        final marqueeRect = Rect.fromPoints(start, end);
-        canvas.drawRect(marqueeRect, paint);
+    return BlocBuilder<CanvasCubit, CanvasState>(
+      buildWhen: (p, c) => p.marqueeEnd != c.marqueeEnd,
+      builder: (context, state) {
+        if (canvas.state.isMarqueeActive) {
+          final start = canvas.toScene(state.marqueeStart!);
+          final end = canvas.toScene(state.marqueeEnd!);
+
+          return CustomPaint(
+            painter: InlinePainter(
+              builder: (canvas, rect) {
+                final paint = Paint()
+                  ..color = colors.secondary.withOpacity(0.3)
+                  ..style = PaintingStyle.fill;
+
+                final marqueeRect = Rect.fromPoints(start, end);
+                canvas.drawRect(marqueeRect, paint);
+              },
+            ),
+          );
+        }
+        return const SizedBox();
       },
-    ));
+    );
   }
 }
