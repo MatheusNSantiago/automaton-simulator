9940e601e85490c6edc151265ba9417c9e7e3815
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-17 18:15:37 -0300

    feat(menuContainer): add new params to the widget

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,8 +4,9 @@ const kSecondaryColor = Color(0xFF100e17);
 const kDarkBackground = Color(0xFF18181b);
 const kButtonColor = Color(0XFF262627);
 const kButtonBorderColor = Color(0XFF828282);
-const kFaintTextColor =  Color(0XFF828282);
+const kButtonBorderRadius = 10.0;
+const kButtonBorderWidth = 0.2;
+const kFaintTextColor = Color(0XFF828282);
 const kNodeColor = Color(0XFF448aff);
 
 const kNodeRadius = 35.0;
-
--- lib/presentation/menus/components/menu_container.dart
+++ lib/presentation/menus/components/menu_container.dart
@@ -1,23 +1,51 @@
 import 'package:automata_simulator/constants.dart';
 import 'package:flutter/material.dart';
 
+// define a type for the option if is a row or a column
+enum MenuContainerType { row, column }
+
 class MenuContainer extends StatelessWidget {
   final List<Widget> children;
+  final double height;
+  final double? width;
+  final MenuContainerType type;
+  final EdgeInsetsGeometry? padding;
 
-  const MenuContainer({ required this.children, super.key, });
+
+  const MenuContainer({
+    required this.children,
+    this.height = 40,
+    this.width,
+    this.type = MenuContainerType.row,
+    this.padding,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40,
+      height: height,
+      width: width,
+      padding: padding,
       decoration: BoxDecoration(
         color: kButtonColor,
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(kButtonBorderRadius),
         border: Border.all(color: kButtonBorderColor, width: 0.2),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            spreadRadius: 0,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
       ),
-      child: Row(
-        children: children,
-        ),
+      child: type == MenuContainerType.row
+          ? Row(children: children)
+          : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: children,
+          ),
     );
   }
 }
